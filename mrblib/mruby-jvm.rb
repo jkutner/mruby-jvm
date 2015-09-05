43d8d37f23e16d28dd96cd096e8a4e7ea55c2470
Joe Kutner <jpkutner@gmail.com>
2015-09-05 09:43:39 -0500

    Added simple entry point

--- mrblib/mruby-jvm.rb
+++ mrblib/mruby-jvm.rb
@@ -1,3 +1,10 @@
 def __main__(argv)
-  puts "NO OP"
+  command = argv.shift
+  java_class = argv.shift
+  java_opts = []
+  while argv.first != "--" and !argv.empty?
+    java_opts << argv.shift
+  end
+  argv.shift
+  JavaSupport.exec_java(java_class, java_opts, argv)
 end
